728d5bf5553b6f890fd361879a9a3e27296e124d
Programmer Blog <programmerblog.net@gmail.com>
2018-11-10 14:34:02 +0400

    Add book insert feature

--- webservice-client.php
+++ webservice-client.php
@@ -8,7 +8,8 @@
 	require_once('lib/nusoap.php');
 	$error  = '';
 	$result = array();
-	$wsdl = "http://localhost/php-webservices/webservice-server.php?wsdl";
+	$response = '';
+	$wsdl = "http://localhost/web-services-in-php/webservice-server.php?wsdl";
 	if(isset($_POST['sub'])){
 		$isbn = trim($_POST['isbn']);
 		if(!$isbn){
@@ -27,6 +28,38 @@
 			 try {
 				$result = $client->call('fetchBookData', array($isbn));
 				$result = json_decode($result);
+			  }catch (Exception $e) {
+			    echo 'Caught exception: ',  $e->getMessage(), "\n";
+			 }
+		}
+	}	
+
+	/* Add new book **/
+	if(isset($_POST['addbtn'])){
+		$title = trim($_POST['title']);
+		$isbn = trim($_POST['isbn']);
+		$author = trim($_POST['author']);
+		$category = trim($_POST['category']);
+		$price = trim($_POST['price']);
+
+		//Perform all required validations here
+		if(!$isbn || !$title || !$author || !$category || !$price){
+			$error = 'All fields are required.';
+		}
+
+		if(!$error){
+			//create client object
+			$client = new nusoap_client($wsdl, true);
+			$err = $client->getError();
+			if ($err) {
+				echo '<h2>Constructor error</h2>' . $err;
+				// At this point, you know the call that follows will fail
+			    exit();
+			}
+			 try {
+				/** Call insert book method */
+				 $response =  $client->call('insertBook', array($title, $author, $price, $isbn, $category));
+				 $response = json_decode($response);
 			  }catch (Exception $e) {
 			    echo 'Caught exception: ',  $e->getMessage(), "\n";
 			 }
@@ -60,7 +93,7 @@
 		<?php } ?>
 	    <div class="form-group">
 	      <label for="email">ISBN:</label>
-	      <input type="text" class="form-control" name="isbn" id="isbn" placeholder="Enter ISBN">
+	      <input type="text" class="form-control" name="isbn" id="isbn" placeholder="Enter ISBN" required>
 	    </div>
 	    <button type="submit" name='sub' class="btn btn-default">Fetch Book Information</button>
     </form>
@@ -78,8 +111,8 @@
       </tr>
     </thead>
     <tbody>
-    <?php if(count($result)){ 
-      		for ($i=0; $i < count($result); $i++) { ?>
+    <?php if($result){ ?>
+      	
 		      <tr>
 		        <td><?php echo $result->title; ?></td>
 		        <td><?php echo $result->author_name; ?></td>
@@ -88,7 +121,6 @@
 		        <td><?php echo $result->category; ?></td>
 		      </tr>
       <?php 
-  			}
   		}else{ ?>
   			<tr>
 		        <td colspan='5'>Enter ISBN and click on Fetch Book Information button</td>
@@ -96,6 +128,41 @@
   		<?php } ?>
     </tbody>
   </table>
+	<div class='row'>
+	<h2>Add New Book</h2>
+	 <?php if(isset($$response->status)) {
+
+	  if($response->status == 200){ ?>
+		<div class="alert alert-success fade in">
+    			<a href="#" class="close" data-dismiss="alert">&times;</a>
+    			<strong>Success!</strong>&nbsp; Book Added succesfully. 
+	        </div>
+	  <?php }elseif(isset($response) && $response->status != 200) { ?>
+			<div class="alert alert-danger fade in">
+    			<a href="#" class="close" data-dismiss="alert">&times;</a>
+    			<strong>Error!</strong>&nbsp; Cannot Add a book. Please try again. 
+	        </div>
+	 <?php } 
+	 }
+	 ?>
+  	<form class="form-inline" method = 'post' name='form1'>
+  		<?php if($error) { ?> 
+	    	<div class="alert alert-danger fade in">
+    			<a href="#" class="close" data-dismiss="alert">&times;</a>
+    			<strong>Error!</strong>&nbsp;<?php echo $error; ?> 
+	        </div>
+		<?php } ?>
+	    <div class="form-group">
+	      <label for="email"></label>
+	      <input type="text" class="form-control" name="title" id="title" placeholder="Enter Title" required>
+				<input type="text" class="form-control" name="author" id="author" placeholder="Enter Author" required>
+				<input type="text" class="form-control" name="price" id="price" placeholder="Enter Price" required>
+				<input type="text" class="form-control" name="isbn" id="isbn" placeholder="Enter ISBN" required>
+				<input type="text" class="form-control" name="category" id="category" placeholder="Enter Category" required>
+	    </div>
+	    <button type="submit" name='addbtn' class="btn btn-default">Add New Book</button>
+    </form>
+   </div>
 </div>
 
 </body>
